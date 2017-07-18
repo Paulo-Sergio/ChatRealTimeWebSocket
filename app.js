9c637bfe25220bf78101fbf4418b527798485d40
Paulo-Sergio <paulofranca.contato@gmail.com>
2017-07-18 15:43:16 -0300

    trabalhando com websocket, comunicação bidirecional entre cliente e servidor... criando e fazendo o chat funcionar de fato

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 /* importar as configurações do servidor */
 var app = require('./config/server');
 
+/* importar modulo do Socket.IO */
+var socketIO = require('socket.io');
+
 /* parametrizar porta de escuta */
-app.listen(3000, function () {
+var server = app.listen(3000, function () {
     console.log('Servidor rodando porta 3000');
-})
+});
+
+var io = socketIO.listen(server);
+
+// deixando a variavel Global para ser acessado na controller -> chat
+app.set('io', io);
+
+/* criar a conexão por websocket */
+// funcao ON vai estar escutando req lado do cliente e vice-versa
+// estamos escutando eventos de connection, evento padrão do socket.io (propria conexao por parametro [socket])
+io.on('connection', function (socket) {
+    console.log('Usuario conectou');
+
+    socket.on('disconnect', function () {
+        console.log('Usuario desconectou');
+    });
+});
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -5,13 +5,20 @@ module.exports.iniciaChat = function (application, req, res) {
 
     /* funcao assert vem do expressValidator (esta no app -> application) */
     req.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty();
-    req.assert('apelido', 'Nome ou apelido deve ter entre 3 até 15 caracteres').len(3, 5);
+    req.assert('apelido', 'Nome ou apelido deve ter entre 3 até 15 caracteres').len(3, 15);
 
     var erros = req.validationErrors();
     if (erros) {
         res.render('index', { validacao: erros });
         return;
     }
+
+    /* emit msgParaCliente (websocket)
+       utilizando obj do socket.io dentro do controller */
+    application.get('io').emit(
+        'msgParaCliente',
+        { apelido: dadosForm.apelido, mensagem: ' acabou de entrar no chat' }
+    );
 
     res.render('chat');
 
